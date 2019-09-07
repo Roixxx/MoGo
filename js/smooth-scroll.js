fc84496365339ac4066419f6fd559e7fac345d58
Max <49519905+Roixxx@users.noreply.github.com>
2019-09-07 14:40:03 +0500

    added smooth scroll function

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,9 +1,19 @@
 /* Smooth Scroll */
 
 $(function() {
+    $("[data-scroll]").on("click", function(event) {
+
+        event.preventDefault();
+
+        var blockId = $(this).data("scroll");
+        var blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    })
 
 
 
 
-    
 });
