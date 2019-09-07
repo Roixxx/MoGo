5cebd4d110473c429f15ba58978332466e174f5f
Max <49519905+Roixxx@users.noreply.github.com>
2019-09-07 14:06:12 +0500

    added checkScroll function

---
+++ app.js
@@ -0,0 +1,22 @@
+$(function() {
+    var header = $("#header");
+    var intorH = $("#intro").innerHeight();
+    var scrollOffset = $(window).scrollTop();
+
+    checkScroll(scrollOffset);
+
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset)
+    });
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= intorH) {
+            header.addClass("header--fixed");
+        } else {
+            header.removeClass("header--fixed");
+        }
+    }
+
+});
