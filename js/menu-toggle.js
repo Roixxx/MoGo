5f43b290f27a1325707cde0eeb1b06bd2d5349e7
Max <49519905+Roixxx@users.noreply.github.com>
2019-09-08 12:40:57 +0500

    Update menu-toggle.js

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -5,6 +5,7 @@ $(function() {
     $("#menu-toggle").on("click", function(event) {
         event.preventDefault();
 
+        $(this).toggleClass("active");
         $("#menu__list").toggleClass("active");
     });
 
