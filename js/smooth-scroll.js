9e379cf41086f4a4633e2456be2d7239412b48ea
Max <49519905+Roixxx@users.noreply.github.com>
2019-09-08 12:22:32 +0500

    added class active

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -5,8 +5,12 @@ $(function() {
 
         event.preventDefault();
 
-        var blockId = $(this).data("scroll");
+        var $this = $(this);
+        var blockId = $this.data("scroll");
         var blockOffset = $(blockId).offset().top;
+
+        $("#menu__list a").removeClass("active");
+        $this.addClass("active");
 
         $("html, body").animate({
             scrollTop: blockOffset
